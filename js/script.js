3f2e4aace2e7fb7ec82766c35e5e9f607e5e516b
Tongei <157807961+Tongei@users.noreply.github.com>
2024-09-11 13:38:45 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ let numberOfRoom = document.querySelector("#numOfRoom");
 document.querySelector("#hotelForm").onsubmit = (event) => {
   event.preventDefault();
   if (validated()) {
-    location = "http://127.0.0.1:5500/pages/hotel.html";
+    location = "/pages/hotel.html";
   }
   document.querySelector("#hotelForm").reset();
 };
